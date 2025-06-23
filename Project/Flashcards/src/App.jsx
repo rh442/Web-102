ad441856ed5ac39cf64940878fd62774bc0efc95
rh442 <hraymond6442@gmail.com>
2025-06-23 18:11:06 -0400

    user input added

--- Project/Flashcards/src/App.jsx
+++ Project/Flashcards/src/App.jsx
@@ -8,16 +8,63 @@ function App() {
   const [currentCard,setCurrentCard] = useState(0);
   const [flip,setFlip] = useState(false);
   const [color,setColor] = useState(false);
-  let card = Flashcards[currentCard];
-
-
-  function getRandomFlashcard() {
-  const randomIndex = Math.floor(Math.random() * (Flashcards.length-1))+1;
-  setCurrentCard(randomIndex);
-  setColor(false)
-  setFlip(false)
+  const [input, setinput] = useState('');
+  const [answerspace, setanswerspace] = useState('');
+  const [correct, setCorrect] = useState(0);
+  
  
-}
+  const [flash_card, setFlashcards] = useState(Flashcards);
+  let card = flash_card[currentCard];
+  
+
+  function handleReset(){
+    setCurrentCard(0);
+    setFlip(false);
+    setColor(false);
+    setCorrect(0);
+  }
+  function handleShuffle(){
+    const shuffled = [...flash_card];
+    for (let i=shuffled.length-1; i>0 ; i--){
+      const j=Math.floor(Math.random()*i)+1;
+      if(i!=currentCard && j!=currentCard){
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    }
+    setFlashcards(shuffled);
+  }
+  function handleSubmit() {
+    if(card.Answer.toLowerCase()==input.toLowerCase()){
+      setanswerspace("Correct");
+      setCorrect(correct+1);
+    }
+    else{
+      setanswerspace("Wrong")
+    }
+  }
+  function handlePreviousClick() {
+    // index = index + 1;
+    if(currentCard>0){
+    setCurrentCard(currentCard - 1);
+    setColor(0);
+    setFlip(0);
+    setanswerspace('');
+    setinput('');
+  }
+  }
+
+  function handleNextClick() {
+    // index = index + 1;
+    if(currentCard<flash_card.length -1){
+    setCurrentCard(currentCard + 1);
+    setColor(0);
+    setFlip(0);
+    setanswerspace('');
+    setinput('');
+    }
+  
+    
+  }
   
  
   function flipCard(){
@@ -25,19 +72,41 @@ function App() {
    setColor(!color);
   }
 
+  
+console.log("Current card image class:", card.img);
   return (
+    <>
+    <div className='overlay'>
+      
+    </div>
     <div className='flashcards'>
-      <h1>Greek and Roman God Names</h1>
+      <h1 className='title'>Greek and Roman God Names</h1>
       <h2>Test your knowledge on the Roman names of the Greek Gods</h2>
-      <div className='card'>
+      <h3 className='progress'>{correct}/{flash_card.length-1}  
+          &nbsp;&nbsp;&nbsp;&nbsp;
+         {((correct/(flash_card.length-1)) * 100).toFixed(2)} %</h3>
+      <div className={card.img}>
         {flip ? <button className='flippable purple' onClick={flipCard}><h1>{card.Answer}</h1></button>:<button className='flippable orange' onClick={flipCard}><h1>{card.Question}</h1></button>}
       </div>
-      <h2>Cards:{Flashcards.length-1}</h2>
-    
-      <button onClick={getRandomFlashcard}>
+
+      <h2>Cards:{flash_card.length-1}</h2>
+      {currentCard>0 &&(
+      <form>
+         <label htmlFor="answer-input">Your Answer:</label>
+        <input id="answer-input" className={answerspace} type='text' value={input} onChange={(e) => setinput(e.target.value)}  ></input>
+        <button type="button" onClick={handleSubmit}>Check answer</button>
+      </form>
+    )}
+      <button onClick={handlePreviousClick} disabled={currentCard=== 0}>
+        Previous
+      </button>
+      <button onClick={handleNextClick} disabled={currentCard===flash_card.length -1}>
         Next
       </button>
+      <button onClick={handleShuffle}>Shuffle🔄</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
+    </>
   )
 }
 
--- Project/Flashcards/src/data.js
+++ Project/Flashcards/src/data.js
@@ -1,55 +1,68 @@
 export const Flashcards = [
 {
     Question: "Ready?",
-    Answer: "Hit next to begin"
+    Answer: "Hit next to begin",
+    img:"card"
 },
 {
     Question: "What is the Roman name of Zeus?",
     Answer: "Jupiter",
+    img:"Zeus"
 },
 {
     Question: "What is the Roman name of Hera?",
     Answer: "Juno",
+    img:"Hera"
 },
 {
     Question: "What is the Roman name of Athena?",
     Answer: "Minerva",
+    img:"Athena"
 },
 {
     Question: "What is the Roman name of Poseidon?",
     Answer: "Neptune",
+    img:"Poseidon"
 },
 {
     Question: "What is the Roman name of Apollo?",
     Answer: "Apollo",
+    img:"Apollo"
 },
 {
     Question: "What is the Roman name of Ares?",
     Answer: "Mars",
+    img:"Ares"
 },
 {
     Question: "What is the Roman name of Demeter?",
     Answer: "Ceres",
+    img:"Demeter"
 },
 {
     Question: "What is the Roman name of Artemis?",
     Answer: "Diana",
+    img:"Artemis"
 },
 {
     Question: "What is the Roman name of Hephaestus?",
     Answer: "Vulcan",
+    img:"Hephaestus"
 },
 {
     Question: "What is the Roman name of Aphrodite?",
     Answer: "Venus",
+    img:"Aphrodite"
 },
 {
     Question: "What is the Roman name of Hermes?",
     Answer: "Mercury",
+    img:"Hermes"
 },
 {
     Question: "What is the Roman name of Dionysus?",
     Answer: "Bacchus",
+    img:"Dionysus"
 },
 
 ];
